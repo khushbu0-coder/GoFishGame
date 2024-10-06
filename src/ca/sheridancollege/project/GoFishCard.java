0ebd9dd266938c80b1fa46a93fb1953a71bdc9dc
Khushbu <khushbu0@sheridancollege.ca>
2024-10-06 14:18:38 -0400

    Update GoFishCard.java
    Defined rank , suit variables using Encapsulation.
    
    Implemented toString method of Card.java

--- src/ca/sheridancollege/project/GoFishCard.java
+++ src/ca/sheridancollege/project/GoFishCard.java
@@ -1,1 +1,26 @@
+package ca.sheridancollege.project;
 
+public class GoFishCard extends Card{
+
+private String rank;      //  2-10 , Jack , Queen , King , Ace 
+private String suit;      //hearts , dimaonds , clubs , spades
+
+public GoFishCard(String rank,String suit){
+  this.rank = rank;
+  this.suit = suit;
+}
+
+public String getrank(){
+  return rank;
+}
+
+public String getsuit(){
+  return suit;
+}
+
+  @Override
+  public String toString(){
+    return rank + "of" + suit;
+  }
+
+}
