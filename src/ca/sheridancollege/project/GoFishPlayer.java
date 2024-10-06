7ffe21711403d491c164a8e9b8e11e13f1cfa444
Khushbu <khushbu0@sheridancollege.ca>
2024-10-06 14:45:13 -0400

    Update GoFishPlayer.java
    hand : declared to add , remove cards from hand .
    score : number of books (one book = 4 cards of same rank)
    
    Used encapsulation 
    created getters to get the access values of variables outside the GoFishPlayer.java
    
    set values of hand, score using constructor

--- src/ca/sheridancollege/project/GoFishPlayer.java
+++ src/ca/sheridancollege/project/GoFishPlayer.java
@@ -1,1 +1,26 @@
+package ca.sheridancollege.project;
 
+public class GoFishPlayer extends Player{
+
+  private ArrayList<GoFishCard>hand;  
+  private int score;  // number of books(collection of 4cards of same rank)
+
+  //Constructor
+
+  public GoFishPlayer(String name){
+    super(name);
+    this.hand = new ArrayList<>();
+    this.score = 0;
+  }
+
+  //Getters
+
+  public ArrayList<GoFishCard> getHand(){
+    return hand;
+  }
+
+  public int getScore(){
+    return score;
+  }
+  
+}
