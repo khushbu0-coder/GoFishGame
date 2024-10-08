5ebc8383eb6f820fab16952c390b6ae908d8356c
Khushbu <khushbu0@sheridancollege.ca>
2024-10-08 09:29:32 -0400

    Added variables and constructor

--- src/ca/sheridancollege/project/GoFishGame.java
+++ src/ca/sheridancollege/project/GoFishGame.java
@@ -1,1 +1,23 @@
+package ca.sheridancollege.project;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class GoFishGame extends Game(){
+  private GroupOfCards deck;
+  private ArrayList<GoFishPlayer>players;
+
+
+public GoFishGame(String name, ArrayList<GoFishPlayer>players){
+  super(name);
+  this.players=players;
+  deck = new GroupOfCards(52);
+  initialize deck();
+
+}
+
+
+
+}
